1d5919b6485fb80fc8d5ae2a972fedae44f299cb
tiberius-ls <ogbonnaemeka543@gmail.com>
2021-02-25 16:04:39 +0100

    2 files modified

--- src/App.js
+++ src/App.js
@@ -3,9 +3,9 @@ import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+      <div>
+        <h1>Hello StackBlitz!</h1>
+        <p>Start editing to see some magic happen :)</p>
+      </div>
   );
 }
